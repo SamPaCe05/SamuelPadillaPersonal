7139012d289905b7e671d9e9bd0b9402dd9b6373
SamPaCe05 <samuelpadilla2605@gmail.com>
2024-09-02 22:48:06 -0500

    JS corregido y CSS, ocultaba la nav de 1024px cuando se usaba en 480px en adelante

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
 
 const menuBar = document.querySelector(".contenedor__menu-mbl")
-
 const nav = document.querySelector(".navegacion")
 
 function eliminarclase() {
-    if (window.innerHeight >= 1024) {
-
+    if (window.innerWidth >= 1024) {
+        console.log("aoskask")
         nav.classList.remove('oculto')
     }
 }
 
+window.addEventListener('resize', eliminarclase())
+window.addEventListener('load', eliminarclase())
+
+function barra() {
+    if (window.innerWidth > 1024) {
+        menuBar.addEventListener('click', () => {
+            menuBar.classList.add('oculto');
+            nav.classList.add('navegacion--activa')
+            nav.classList.remove('oculto')
+        })
+        const links = document.querySelectorAll('.navegacion a');
+        links.forEach(link => {
+            link.addEventListener('click', e => {
+                menuBar.classList.remove('oculto')
+                nav.classList.add('oculto')
+                nav.classList.remove('navegacion--activa')
+            })
+        })
+    }
+}
+window.addEventListener('click', barra());
 
 
-menuBar.addEventListener('click', () => {
-
-    menuBar.classList.add('oculto');
-
-    nav.classList.add('navegacion--activa')
-    nav.classList.remove('oculto')
-})
-
-
-const links = document.querySelectorAll('.navegacion a');
-links.forEach(link => {
-
-    link.addEventListener('click', e => {
-        menuBar.classList.remove('oculto')
-        nav.classList.add('oculto')
-        nav.classList.remove('navegacion--activa')
-
-    })
-
-})
